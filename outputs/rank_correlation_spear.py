8e86bbee9cc71b03e6703d22e3a4fb2ae779fff8
Jyotiraditya <yjyotiraditya@gmail.com>
2025-01-15 01:35:42 +0530

    rank correlation added

--- outputs/rank_correlation_spear.py
+++ outputs/rank_correlation_spear.py
@@ -2,7 +2,14 @@ import pandas as pd
 from scipy.stats import spearmanr
 
 # Read the CSV file
-df = pd.read_csv("malabar_similarities.csv")
+df = pd.read_csv("india_similarities.csv")
+
+# Add ranks for columns used in rank correlation
+df['Rank_Euclidean_Similarity'] = df['Euclidean_Similarity'].rank()
+df['Rank_Average_Probability_Euclidean'] = df['Average_Probability'].rank()
+
+df['Rank_Cosine_Similarity'] = df['Cosine_Similarity'].rank()
+df['Rank_Average_Probability_Cosine'] = df['Average_Probability'].rank()
 
 # Compute Spearman rank correlation for each pair
 rank_corr_euclidean, _ = spearmanr(df['Euclidean_Similarity'], df['Average_Probability'])
@@ -12,5 +19,7 @@ rank_corr_cosine, _ = spearmanr(df['Cosine_Similarity'], df['Average_Probability
 df['Rank_Correlation_Euclidean'] = rank_corr_euclidean
 df['Rank_Correlation_Cosine'] = rank_corr_cosine
 
-# Display the updated DataFrame
-df.to_csv("malabar_similarities_rank_correlation.csv", index=False)
+# Save the updated DataFrame to a new CSV file
+df.to_csv("india_similarities_rank_correlation.csv", index=False)
+
+
